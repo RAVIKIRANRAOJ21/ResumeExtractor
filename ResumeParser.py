c80824fab0b1958615f7c63aa143f588efef5cc3
RAVIKIRANRAOJ21 <42383903+RAVIKIRANRAOJ21@users.noreply.github.com>
2020-01-20 17:25:35 +0530

    Update ResumeParser.py

--- ResumeParser.py
+++ ResumeParser.py
@@ -6,6 +6,7 @@ import re
 
 def email(path):
     tmp = doc2txt(path)
+#     tmp = pdf2txt(path)
     pattern=re.compile(r'[a-zA-Z0-9-\.]+@+[a-zA-Z0-9-\.]*')
     matches=pattern.finditer(tmp)
     for i in matches:
