074a4718a43c546ea6f8d64553f009e85e685779
RAVIKIRANRAOJ21 <42383903+RAVIKIRANRAOJ21@users.noreply.github.com>
2020-01-20 15:11:38 +0530

    Update pdf2txt.py

--- pdf2txt.py
+++ pdf2txt.py
@@ -1,13 +1,13 @@
 import PyPDF2
 
-fl = open('C://Users//kiran//Desktop//resume.pdf', 'rb')
+def pdf2txt(path):
+     fl = open(path, 'rb')
+     pdfReader = PyPDF2.PdfFileReader(fl)
+     nop = pdfReader.numPages
+     for i in range(nop):
+          pageObj = pdfReader.getPage(i)
+          extractedtxt=pageObj.extractText()
+     return extractedtxt
+     fl.close()
 
-pdfReader = PyPDF2.PdfFileReader(fl)
-
-nop = pdfReader.numPages
-
-for i in range(nop):
-     pageObj = pdfReader.getPage(i)
-     print(pageObj.extractText())
-
-fl.close()
+print (pdf2txt('C://Users//kiran//Desktop//resume.pdf'))
