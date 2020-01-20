dee53da76a86b4bd89347a58863447be4f0805ff
RAVIKIRANRAOJ21 <42383903+RAVIKIRANRAOJ21@users.noreply.github.com>
2020-01-20 17:23:14 +0530

    Create ResumeParser.py

---
+++ ResumeParser.py
@@ -0,0 +1,15 @@
+from doc2txt import doc2txt
+from pdf2txt import pdf2txt
+
+import re
+
+
+def email(path):
+    tmp = doc2txt(path)
+    pattern=re.compile(r'[a-zA-Z0-9-\.]+@+[a-zA-Z0-9-\.]*')
+    matches=pattern.finditer(tmp)
+    for i in matches:
+        j=i.group(0)
+    return j
+
+print(email('C://Users//kiran//Desktop//resume.docx'))
