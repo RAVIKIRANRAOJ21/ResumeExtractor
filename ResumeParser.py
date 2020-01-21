1e5fdc98e3d9d7f0d5fe715a69e2a3455b5dc864
RAVIKIRANRAOJ21 <42383903+RAVIKIRANRAOJ21@users.noreply.github.com>
2020-01-21 14:19:55 +0530

    Update ResumeParser.py

--- ResumeParser.py
+++ ResumeParser.py
@@ -8,9 +8,22 @@ def email(path):
     tmp = doc2txt(path)
 #     tmp = pdf2txt(path)
     pattern=re.compile(r'[a-zA-Z0-9-\.]+@+[a-zA-Z0-9-\.]*')
-    matches=pattern.finditer(tmp)
+    matches=pattern.findall(tmp)
+    mail = ''
     for i in matches:
-        i.group()
-    return i.group()
+        mail = mail + '\n' + i
+    return mail
 
-print('Email = ' + email('C://Users//kiran//Desktop//resume.docx'))
+def PhoneNo(path):
+    tmp = doc2txt(path)
+#     tmp = pdf2txt(path)
+    pattern = re.compile(r'(\d\d\d\d\d\d\d\d\d\d)')  #  re (phoneno.) logic needs to be improved
+    matches = pattern.findall(tmp)
+    Number = ''
+    for x in matches:
+        Number = Number + '\n' + x
+    return Number
+
+print(" Phone Numbers : " + PhoneNo('C://Users//kiran//Desktop//resume.pdf'))
+
+print('Email : ' + email('C://Users//kiran//Desktop//resume.docx'))
