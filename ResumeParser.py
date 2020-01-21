8a8c1a02352f7c9b79c105527fc8c3e6062ab22c
RAVIKIRANRAOJ21 <42383903+RAVIKIRANRAOJ21@users.noreply.github.com>
2020-01-21 21:32:42 +0530

    Update ResumeParser.py

--- ResumeParser.py
+++ ResumeParser.py
@@ -2,7 +2,7 @@ from doc2txt import doc2txt
 from pdf2txt import pdf2txt
 
 import re
-
+import string
 
 def email(path):
     tmp = doc2txt(path)
@@ -32,8 +32,28 @@ def experience(path):
     for x in matches:
         return x.group()
 
-print("Experience : " + experience('C://Users//kiran//Desktop//Resume3.pdf'))
+def Name(path):
+    my_text=pdf2txt(path)
+    # my_text=doc2txt(path)
+    from nltk.tokenize import word_tokenize
+    from nltk.corpus import stopwords
+    #resume = getText(Nikhilresume).encode("ascii", "ignore")
+    #print ("tokenizing the given file ......")
+    tokens = word_tokenize(my_text)
+    punctuations = ['(',')',';',':','[',']',',']
+    stop_words = stopwords.words('english')
+    #storing the cleaned resume
+    filtered = [w for w in tokens if not w in stop_words and  not w in string.punctuation]
+    #print ("removing the stop words....\nCleaning the resumes....\nExtracting Text .......")
+    #print (filtered)
+    #get the name from the resume
+    name  = str(filtered[0])+' ' +str(filtered[1])
+    print ("Name : "+ name)
 
-print("Phone Numbers : " + PhoneNo('C://Users//kiran//Desktop//resume.pdf'))
+# print(Name('C://Users//kiran//Desktop//resume.pdf'))    
 
-print("Email : " + email('C://Users//kiran//Desktop//resume.docx'))
+# print("Experience : " + experience('C://Users//kiran//Desktop//Resume3.pdf'))
+
+# print("Phone Numbers : " + PhoneNo('C://Users//kiran//Desktop//resume.pdf'))
+
+# print("Email : " + email('C://Users//kiran//Desktop//resume.docx'))
