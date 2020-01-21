5f3060ff098eafd668225a902a7e23064187b1f4
RAVIKIRANRAOJ21 <42383903+RAVIKIRANRAOJ21@users.noreply.github.com>
2020-01-21 21:51:31 +0530

    Update ResumeParser.py

--- ResumeParser.py
+++ ResumeParser.py
@@ -1,12 +1,21 @@
 from doc2txt import doc2txt
 from pdf2txt import pdf2txt
-
-import re
 import string
+import re
+
+def PhoneNo(path):
+    tmp = doc2txt(path)
+    # tmp = pdf2txt(path)
+    pattern = re.compile(r'(\d\d\d\d\d\d\d\d\d\d)')  # re logic needs to be improved
+    matches = pattern.findall(tmp)
+    Number = ''
+    for x in matches:
+        Number = Number + '\n' + x
+    return Number
 
 def email(path):
     tmp = doc2txt(path)
-#     tmp = pdf2txt(path)
+    # tmp = pdf2txt(path)
     pattern=re.compile(r'[a-zA-Z0-9-\.]+@+[a-zA-Z0-9-\.]*')
     matches=pattern.findall(tmp)
     mail = ''
@@ -14,27 +23,19 @@ def email(path):
         mail = mail + '\n' + i
     return mail
 
-def PhoneNo(path):
-    tmp = doc2txt(path)
-#     tmp = pdf2txt(path)
-    pattern = re.compile(r'(\d\d\d\d\d\d\d\d\d\d)')  #  re (phoneno.) logic needs to be improved
-    matches = pattern.findall(tmp)
-    Number = ''
-    for x in matches:
-        Number = Number + '\n' + x
-    return Number
-
 def experience(path):
     my_text = doc2txt(path)
-#     my_text= pdf2txt(path)
+    # my_text= pdf2txt(path)
     pattern=re.compile(r"(\d+(?:-\d+)?\+?)\s*(years?)", re.I)
+    y = 'None'
     matches=pattern.finditer(my_text)
     for x in matches:
-        return x.group()
+        y = x.group()
+    return y
 
 def Name(path):
-    my_text=pdf2txt(path)
-    # my_text=doc2txt(path)
+    my_text=doc2txt(path)
+    # my_text=pdf2txt(path)
     from nltk.tokenize import word_tokenize
     from nltk.corpus import stopwords
     #resume = getText(Nikhilresume).encode("ascii", "ignore")
@@ -48,12 +49,11 @@ def Name(path):
     #print (filtered)
     #get the name from the resume
     name  = str(filtered[0])+' ' +str(filtered[1])
-    print ("Name : "+ name)
+    return name
 
-# print(Name('C://Users//kiran//Desktop//resume.pdf'))    
+Path = 'C://Users//kiran//Desktop//resume.docx'
 
-# print("Experience : " + experience('C://Users//kiran//Desktop//Resume3.pdf'))
-
-# print("Phone Numbers : " + PhoneNo('C://Users//kiran//Desktop//resume.pdf'))
-
-# print("Email : " + email('C://Users//kiran//Desktop//resume.docx'))
+print("Name : " + Name(Path))
+print("Email : " + email(Path))
+print("Phone Numbers : " + PhoneNo(Path))
+print("Experience : " + experience(Path))
