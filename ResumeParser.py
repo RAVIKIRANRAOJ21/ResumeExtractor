00b578b1eaf7160126de503b26230fc9549d202b
RAVIKIRANRAOJ21 <42383903+RAVIKIRANRAOJ21@users.noreply.github.com>
2020-01-22 13:16:44 +0530

    skills extraction has been added to the code
    libraries used for this are spacy, pandas and inbuilt os libraries.

--- ResumeParser.py
+++ ResumeParser.py
@@ -1,7 +1,10 @@
-from doc2txt import doc2txt
-from pdf2txt import pdf2txt
 import string
 import re
+import spacy
+import pandas as panda
+import os
+from doc2txt import doc2txt
+from pdf2txt import pdf2txt
 
 def PhoneNo(path):
     tmp = doc2txt(path)
@@ -48,8 +51,26 @@ def Name(path):
     #print ("removing the stop words....\nCleaning the resumes....\nExtracting Text .......")
     #print (filtered)
     #get the name from the resume
-    name  = str(filtered[0])+' ' +str(filtered[1])
+    name  = str(filtered[0])+' ' +str(filtered[1])        # logic is wrong, need to be improved.
     return name
+
+def skills(path):
+    nlp = spacy.load('en_core_web_sm')
+    my_text = nlp(doc2txt(path))
+    # my_text = nlp(pdf2txt(path))
+    nounchunks = list(my_text.noun_chunks)
+    tokens = [token.text for token in my_text if not token.is_stop]
+    data = panda.read_csv(os.path.join(os.path.dirname(__file__), 'technical_skills.csv'))
+    skills = list(data.columns.values)
+    skill_set = []
+    for token in tokens:
+        if token.lower() in skills:
+            skill_set.append(token)
+    for token in nounchunks:
+        token = token.text.lower().strip()
+        if token in skills:
+            skill_set.append(token)
+    return [i.capitalize() for i in set([i.lower() for i in skill_set])]
 
 Path = 'C://Users//kiran//Desktop//resume.docx'
 
@@ -57,3 +78,4 @@ print("Name : " + Name(Path))
 print("Email : " + email(Path))
 print("Phone Numbers : " + PhoneNo(Path))
 print("Experience : " + experience(Path))
+print("Skills : " + str(skills(Path)))
