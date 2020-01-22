58de759fdad54d91446b16db405202ab973e9037
RAVIKIRANRAOJ21 <42383903+RAVIKIRANRAOJ21@users.noreply.github.com>
2020-01-22 18:36:53 +0530

    Update ResumeParser.py
    updated the code of name, changed the regex of phone no. and included DOB

--- ResumeParser.py
+++ ResumeParser.py
@@ -1,20 +1,22 @@
-import string
 import re
 import spacy
 import pandas as panda
 import os
 from doc2txt import doc2txt
 from pdf2txt import pdf2txt
+from spacy.matcher import Matcher
 
 def PhoneNo(path):
     tmp = doc2txt(path)
-    # tmp = pdf2txt(path)
-    pattern = re.compile(r'(\d\d\d\d\d\d\d\d\d\d)')  # re logic needs to be improved
-    matches = pattern.findall(tmp)
-    Number = ''
-    for x in matches:
-        Number = Number + '\n' + x
-    return Number
+    # text = pdf2txt(path)
+    pattern = re.compile(r'(?:(?:\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9])\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([0-9][1-9]|[0-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{5})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?')
+    match = pattern.findall(text)
+    if match:
+        number = ''.join(match[0])
+        if len(number) > 10:
+            return '+' + number
+        else:
+            return number
 
 def email(path):
     tmp = doc2txt(path)
@@ -30,29 +32,22 @@ def experience(path):
     my_text = doc2txt(path)
     # my_text= pdf2txt(path)
     pattern=re.compile(r"(\d+(?:-\d+)?\+?)\s*(years?)", re.I)
-    y = 'None'
     matches=pattern.finditer(my_text)
     for x in matches:
-        y = x.group()
-    return y
+        return x.group()
 
 def Name(path):
-    my_text=doc2txt(path)
-    # my_text=pdf2txt(path)
-    from nltk.tokenize import word_tokenize
-    from nltk.corpus import stopwords
-    #resume = getText(Nikhilresume).encode("ascii", "ignore")
-    #print ("tokenizing the given file ......")
-    tokens = word_tokenize(my_text)
-    punctuations = ['(',')',';',':','[',']',',']
-    stop_words = stopwords.words('english')
-    #storing the cleaned resume
-    filtered = [w for w in tokens if not w in stop_words and  not w in string.punctuation]
-    #print ("removing the stop words....\nCleaning the resumes....\nExtracting Text .......")
-    #print (filtered)
-    #get the name from the resume
-    name  = str(filtered[0])+' ' +str(filtered[1])        # logic is wrong, need to be improved.
-    return name
+     nlp = spacy.load('en_core_web_sm')
+    my_text = nlp(doc2txt(path))
+    # my_text = nlp(pdf2txt(path))
+    NAME_PATTERN = [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
+    match = Matcher(nlp.vocab)
+    pattern = [NAME_PATTERN]
+    match.add('NAME', None, *pattern)
+    matches = match(my_text)
+    for match_id, start, end in matches:
+        span = my_text[start:end]
+        return span.text
 
 def skills(path):
     nlp = spacy.load('en_core_web_sm')
@@ -72,10 +67,19 @@ def skills(path):
             skill_set.append(token)
     return [i.capitalize() for i in set([i.lower() for i in skill_set])]
 
+def DOB(path):
+    my_text = doc2txt(path)
+    # my_text = pdf2txt(path)
+    pattern = re.compile(r'([0-3]?[0-9](?:\.|\/|\-|\s)[0-3]?(?:[0-9]|' + r'(?:Feb|Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec|January' + r'|February|March|April|May|June|July|August|September|October|' +r'November|December))(?:\.|\/|\-|\s)(?:[0-9]{2})?[0-9]{2})',re.IGNORECASE | re.UNICODE)
+    matches=pattern.finditer(my_text)
+    for x in matches:
+         return x.group()
+
 Path = 'C://Users//kiran//Desktop//resume.docx'
 
-print("Name : " + Name(Path))
+print("Name : " + str(Name(Path)))
 print("Email : " + email(Path))
 print("Phone Numbers : " + PhoneNo(Path))
 print("Experience : " + experience(Path))
 print("Skills : " + str(skills(Path)))
+print("Date of Birth : " + DOB(Path))
